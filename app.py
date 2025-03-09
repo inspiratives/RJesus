d12da010c8166706bcb258b0d561411de9cba9ac
Yongjun Park <shaneangel@naver.com>
2025-03-10 00:07:24 +0900

    화면 디자인 보완 / 본문해설 추가

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title('리딩지저스')
+
+# st.title('리딩지저스')
 
 # read 'url_list.csv'
 import pandas as pd
@@ -12,16 +13,30 @@ today = datetime.datetime.now() + datetime.timedelta(days=1)
 today = today.strftime('%Y-%m-%d')
 
 # get today data from url_list
-today_data = url_list[url_list['date'] == today].to_dict('records')[0]
-# st.write(today_data)
+today_data = url_list[url_list['date'] == today]
+data = url_list[url_list['date'] == today].to_dict('records')[0]
+volume = int(data["volume"])
+chapter = int(data["chapter"])
+day = int(data["day"])
+# st.write(data)
 
 # display today data (title, )
-st.subheader(today_data['title'])
-st.caption(f'{today_data["volume"]}권 {today_data["chapter"]}강 {today_data["day"]}일차')
+st.progress(today_data.index[0] / len(url_list), f'📖 {round(today_data.index[0] / len(url_list)*100, 1)}%')
+st.markdown("<h1 style='text-align: center;'>&nbsp;&nbsp;&nbsp;리딩지저스</h1>", unsafe_allow_html=True)
+# st.divider()
+# st.subheader(data['title'])
+if day != 0:
+    st.caption(f'{volume}권 {chapter}강 {day}일차')
+else:
+    st.caption(f'{volume}권 {chapter}강')
 
+if day != 0:
+    with st.expander('**본문해설**', expanded=True):
+        st.image(f'Summary/{volume}권 성경읽기/{volume}권 {chapter}강/{volume}권{chapter}강_성경읽기_{day}.jpg')
+    
 # display today data (youtube)
-st.video(today_data['url'])
+st.video(data['url'])
 
 # sidebar
-st.sidebar.title('진행표')
+# st.sidebar.title('진행표')
 # st.sidebar.table(st.dataframe())
